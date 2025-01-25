0419c8903e0e9656e4c1cb0dd81f2422bc279500
Ganonymous <andrewleetham@gmail.com>
2025-01-24 22:23:29 -0600

    Data-driven messageList, allow message adding

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -8,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.component.css'
 })
 export class MessageEditComponent {
+  @ViewChild('subject', {static: false}) subjectInputRef: ElementRef;
+  @ViewChild('msgText', {static: false}) msgInputRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+
+  currentSender = 'Andrew Leetham';
+
+  onSendMessage(){
+    const subject = this.subjectInputRef.nativeElement.value;
+    const msgText = this.msgInputRef.nativeElement.value;
+    this.addMessageEvent.emit(new Message(3, subject, msgText, this.currentSender));
+  }
+
+  onClear(){
+    this.subjectInputRef.nativeElement.value = '';
+    this.msgInputRef.nativeElement.value = '';
+  }
 
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.component.css'
 })
 export class MessageListComponent {
+  messages: Message[] = [
+    new Message(0, 'Hello World', 'This is the first message', 'Everyone'),
+    new Message(1, 'Welcome to Class!', 'Welcome to the class, everyone!', 'David Del Sol'),
+    new Message(2, 'What is foo/bar?', 'All the tutorials keep calling this "foo" function, what does it do?', 'Connie Confusion')
+  ];
 
+  onAddMessage(message: Message){
+    this.messages.push(message);
+  }
 }
