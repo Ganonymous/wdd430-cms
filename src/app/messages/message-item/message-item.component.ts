fc2fd6af7839c60110799f3dbfe680e88a3e4921
Ganonymous <andrewleetham@gmail.com>
2025-02-09 00:28:42 -0800

    Remove debug logging

--- src/app/messages/message-item/message-item.component.ts
+++ src/app/messages/message-item/message-item.component.ts
@@ -17,9 +17,7 @@ export class MessageItemComponent implements OnInit{
   constructor(private contactService: ContactService) {};
 
   ngOnInit() {
-    console.log(this.message.sender);
      const contact: Contact = this.contactService.getContact(this.message.sender);
-     console.log(contact);
      this.messageSender = contact.name;
   }
 }
