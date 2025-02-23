4cf7a40275ea6b4110f0f18fd89d3c13efeba3b9
Ganonymous <andrewleetham@gmail.com>
2025-02-22 16:58:21 -0800

    Create add & update functions for documents and contacts

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -8,10 +8,22 @@ import { Subject } from 'rxjs';
 })
 export class ContactService {
   contactListChangedEvent = new Subject<Contact[]>();
-
+  maxContactId: number;
   contacts: Contact[] = [];
   constructor() {
     this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
+  }
+
+  getMaxId(): number{
+    let maxId = 0;
+
+    for(let contact of this.contacts){
+      const currentId = +contact.id;
+      if(currentId > maxId){maxId = currentId}
+    }
+
+    return maxId;
   }
 
   getContacts(): Contact[] {
@@ -24,6 +36,27 @@ export class ContactService {
       if(contact.id == id) returnVal = contact;
     });
     return returnVal;
+  }
+
+  addContact(newContact: Contact){
+    if(!newContact){return}
+
+    this.maxContactId++;
+    newContact.id = `${this.maxContactId}`;
+    this.contacts.push(newContact);
+    this.contactListChangedEvent.next(this.contacts.slice());
+  }
+
+  updateContact(originalContact: Contact, newContact: Contact){
+    if(!originalContact || !newContact){return}
+
+    const pos = this.contacts.indexOf(originalContact);
+
+    if(pos < 0){return}
+
+    newContact.id = originalContact.id;
+    this.contacts[pos] = newContact;
+    this.contactListChangedEvent.next(this.contacts.slice());
   }
 
   deleteContact(contact: Contact){
--- src/app/documents/document.service.ts
+++ src/app/documents/document.service.ts
@@ -8,11 +8,23 @@ import { Subject } from 'rxjs';
 })
 export class DocumentService {
   documents: Document[] = [];
-
+  maxDocumentId: number;
   documentListChangedEvent = new Subject<Document[]>();
 
   constructor() {
     this.documents = MOCKDOCUMENTS;
+    this.maxDocumentId = this.getMaxId();
+  }
+
+  getMaxId(): number{
+    let maxId = 0;
+
+    for(let document of this.documents){
+      let currentId = +document.id;
+      if(currentId > maxId){ maxId = currentId; }
+    }
+
+    return maxId;
   }
 
   getDocuments(): Document[] {
@@ -25,6 +37,27 @@ export class DocumentService {
       if(document.id == id) returnVal = document;
     });
     return returnVal;
+  }
+
+  addDocument(newDocument: Document){
+    if(!newDocument){return}
+
+    this.maxDocumentId++;
+    newDocument.id = `${this.maxDocumentId}`;
+    this.documents.push(newDocument);
+    this.documentListChangedEvent.next(this.documents.slice());
+  }
+
+  updateDocument(originalDocument: Document, newDocument: Document){
+    if(!originalDocument || !newDocument){return}
+
+    const pos = this.documents.indexOf(originalDocument);
+
+    if(pos < 0){return}
+
+    newDocument.id = originalDocument.id;
+    this.documents[pos] = newDocument;
+    this.documentListChangedEvent.next(this.documents.slice());
   }
 
   deleteDocument(document: Document){
